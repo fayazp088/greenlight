c5e36798d08a5ecdeca7d499baee2fb121d2aeee
fayaz <fayazp088@gmail.com>
2023-12-30 22:38:31 +0530

    initial health check setup for greenlight project

---
+++ cmd/api/movies.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Create a new movie handler!!!")
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+
+	id, err := app.readIDParams(r)
+
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "show the details of movie %d\n", id)
+
+}
