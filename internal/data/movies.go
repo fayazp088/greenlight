2b48641591c79cffa9578a4ad8d8608130366d57
fayaz <fayazp088@gmail.com>
2024-01-01 22:09:58 +0530

    adding filters

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/fayazp088/greenlight/internal/validator"
@@ -155,26 +156,37 @@ func (m *MovieModel) Delete(id int64) error {
 	return nil
 }
 
-func (m *MovieModel) GetAll() ([]*Movie, error) {
-	query := `SELECT id, title, year, created_at, runtime, genres, version
+func (m *MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, Metadata, error) {
+	query := fmt.Sprintf(`SELECT 
+		count(*) OVER(), id, title, year, created_at, runtime, genres, version
 		FROM movies
-		ORDER BY id
-	`
+		WHERE 
+		(to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '') 
+			AND 
+		(genres @> $2 OR $2 = '{}')
+		ORDER BY %s %s, id ASC
+		LIMIT $3 OFFSET $4
+	`, filters.sortColumn(), filters.sortDirection())
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	rows, err := m.DB.QueryContext(ctx, query)
+	args := []any{title, pq.Array(genres), filters.limit(), filters.offset()}
+
+	rows, err := m.DB.QueryContext(ctx, query, args...)
 
 	if err != nil {
-		return nil, err
+		return nil, Metadata{}, err
 	}
+	defer rows.Close()
 
+	totalRecords := 0
 	movies := []*Movie{}
 
 	for rows.Next() {
 		var movie Movie
 		err = rows.Scan(
+			&totalRecords,
 			&movie.ID,
 			&movie.Title,
 			&movie.Year,
@@ -185,15 +197,16 @@ func (m *MovieModel) GetAll() ([]*Movie, error) {
 		)
 
 		if err != nil {
-			return nil, err
+			return nil, Metadata{}, err
 		}
 
 		movies = append(movies, &movie)
 	}
 
 	if err = rows.Err(); err != nil {
-		return nil, err
+		return nil, Metadata{}, err
 	}
 
-	return movies, nil
+	metaData := calculateMetadata(totalRecords, filters.Page, filters.PageSize)
+	return movies, metaData, nil
 }
